6168bf99b89c24e6ef72091c65874e7cbacd82c0
wumuch <wumuch@gmail.com>
2015-09-27 16:25:53 +0800

    revision

--- app/src/main/java/com/wumch/pecar/PecarService.java
+++ app/src/main/java/com/wumch/pecar/PecarService.java
@@ -22,18 +22,16 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.ShortBufferException;
 
-
 public class PecarService extends VpnService implements Handler.Callback, Runnable
 {
     private final int authTimeout = 60000;
-
-    private Crypto crypto;
+    private final int workInterval = 50;
 
     private String serverAddress;
     private String serverPort;
     private byte[] username, password;
-    private PendingIntent configIntent;
 
+    private Crypto crypto;
     private Handler handler;
     private Thread thread;
 
@@ -51,26 +49,19 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
-        // The handler is only used to show messages.
+        destroy();
+
         if (handler == null) {
             handler = new Handler(this);
         }
-
-        // Stop the previous session by interrupting the thread.
-        if (thread != null) {
-            thread.interrupt();
-        }
-
-        // Extract information from the intent.
         String prefix = getPackageName();
         serverAddress = intent.getStringExtra(prefix + ".ADDRESS");
         serverPort = intent.getStringExtra(prefix + ".PORT");
         username = intent.getStringExtra(prefix + ".USERNAME").getBytes();
         password = intent.getStringExtra(prefix + ".PASSWORD").getBytes();
-
         fillDefaultParam();
 
-        thread = new Thread(this, "ToyVpnThread");
+        thread = new Thread(this, "PecarThread");
         thread.start();
         foregroundStart();
         return START_STICKY;
@@ -110,9 +101,7 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
     @Override
     public void onDestroy()
     {
-        if (thread != null) {
-            thread.interrupt();
-        }
+        destroy();
     }
 
     @Override
@@ -132,34 +121,30 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
             Log.i(getText(R.string.log_tag).toString(), "server: [" + serverAddress + ":" + Integer.toString(Integer.parseInt(serverPort)) + "]");
             handler.sendEmptyMessage(R.string.connecting);
             run(server);
-            Log.i(getText(R.string.log_tag).toString(), "Giving up");
+            Log.i(getText(R.string.log_tag).toString(), "run out");
         } catch (Exception e) {
             Log.e(getText(R.string.log_tag).toString(), "runserver(server) got " + e.toString());
         } finally {
             try {
                 ifd.close();
             } catch (IOException e) {
-                // ignore
+                Log.i(getText(R.string.log_tag).toString(), "run()->finally->catch(IOException): " + e.getMessage());
             } finally {
                 ifd = null;
                 handler.sendEmptyMessage(R.string.disconnected);
-                Log.i(getText(R.string.log_tag).toString(), "Exiting");
+                Log.i(getText(R.string.log_tag).toString(), "run()->finally->finanlly");
             }
         }
     }
 
-    private boolean run(InetSocketAddress server)
+    private void run(InetSocketAddress server)
     {
         crypto = new Crypto(getText(R.string.log_tag).toString());
-        boolean connected = false;
         try {
-            if (prepareTunnel(server)) {
-                connected = true;
+            if (prepareUpstream(server)) {
                 handler.sendEmptyMessage(R.string.connected);
                 configure();
                 work();
-            } else {
-                connected = false;
             }
         } catch (IOException e) {
             // TODO: maybe network error, notify user.
@@ -173,13 +158,23 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
                 }
                 ifd.close();
             } catch (IOException e) {
-                // ignore
+                Log.e(getText(R.string.log_tag).toString(), "exception (run(InetSocketAddress)->finally->catch(IOException)): " + e.toString());
             }
         }
-        return connected;
     }
 
-    private boolean prepareTunnel(InetSocketAddress serverAddr) throws IOException
+    private PendingIntent getConfigIntent()
+    {
+        // make the configure Button show the Log
+        Intent intent = new Intent(getBaseContext(), PecarClient.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        PendingIntent startLW = PendingIntent.getActivity(this, 0, intent, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        return startLW;
+
+    }
+
+    private boolean prepareUpstream(InetSocketAddress serverAddr) throws IOException
     {
         upstream = SocketChannel.open();
         if (!protect(upstream.socket())) {
@@ -190,7 +185,8 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
         try {
             return handshake();
         } catch (Exception e) {
-            Log.i(getText(R.string.log_tag).toString(), "exception from handshake" );
+            Log.i(getText(R.string.log_tag).toString(), e.getClass().toString() +
+                "prepareUpstream(InetSocketAddress)->try->catch(Exception): " + e.getMessage());
             return false;
         }
     }
@@ -200,17 +196,19 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
         FileInputStream dsr = new FileInputStream(ifd.getFileDescriptor());
         FileOutputStream dsw = new FileOutputStream(ifd.getFileDescriptor());
 
-        byte[] dr = new byte[32767];
-        byte[] dw = new byte[32767];
-        byte[] ur = new byte[32767];
-        byte[] uw = new byte[32767];
+        byte[] dr = new byte[32767],
+            dw = new byte[32767],
+            ur = new byte[32767],
+            uw = new byte[32767];
 
         while (true) {
             int len = dsr.read(dr);
             if (len > 0) {
                 crypto.encrypt(dr, len, uw);
                 int written = upstream.write(ByteBuffer.wrap(uw, 0, len));
-                Log.i(getText(R.string.log_tag).toString(), "upstream written: " + Integer.toString(written));
+                int written2 = upstream.write(ByteBuffer.wrap(uw, 0, len));     // TODO: test only
+                int written3 = upstream.write(ByteBuffer.wrap(uw, 0, len));     // TODO: test only
+                int written4 = upstream.write(ByteBuffer.wrap(uw, 0, len));     // TODO: test only
             }
 
             len = upstream.read(ByteBuffer.wrap(ur, 0, ur.length));
@@ -218,7 +216,7 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
                 crypto.decrypt(ur, len, dw);
                 dsw.write(dw, 0, len);
             }
-            Thread.sleep(50);
+            Thread.sleep(workInterval);
         }
     }
 
@@ -226,62 +224,64 @@ public class PecarService extends VpnService implements Handler.Callback, Runnab
     {
         ByteBuffer packet = ByteBuffer.allocate(128);
         packet.position(crypto.genKeyIvList(packet.array()));
-        Log.i(getText(R.string.log_tag).toString(), "position: " + Integer.toString(packet.position()));
-        packet.put((byte)username.length);
+        packet.put((byte) username.length);
         packet.put((byte)password.length);
         packet.put(username);
         packet.put(password);
         packet.flip();
-        int written = upstream.write(packet);
-        Log.i(getText(R.string.log_tag).toString(), "written: " + Integer.toString(written));
+        upstream.write(packet);
 
         packet.clear();
         final int interval = 100;
         for (int i = 0, end = (int)Math.ceil(authTimeout / interval); i < end; ++i) {
             Thread.sleep(interval);
             packet.limit(1);
-            int len = upstream.read(packet);
-            if (len > 0) {
+            if (upstream.read(packet) > 0) {
                 packet.position(0);
-                int authRes = packet.get(0) & 0xff;
-                Log.i(getText(R.string.log_tag).toString(), "authRes: " + authRes + ", Rescode.OK: " + ResCode.OK);
-                if (authRes == 0) {
-                    Log.i(getText(R.string.log_tag).toString(), "authRes == 0" );
+                switch (packet.get(0))
+                {
+                case ResCode.OK:
                     return true;
-                } else if (authRes == 1) {
-                    Log.i(getText(R.string.log_tag).toString(), "authRes == 1" );
+                case ResCode.NONEXISTS:
                     return false;
-                } else if (authRes == 2) {
-                    Log.i(getText(R.string.log_tag).toString(), "authRes == 2" );
+                case ResCode.EXPIRED:
                     return false;
-                } else if (authRes == 3) {
-                    Log.i(getText(R.string.log_tag).toString(), "authRes == 3" );
+                case ResCode.TRAFFIC_EXHAUST:
                     return false;
-                } else {
-                    Log.i(getText(R.string.log_tag).toString(), "authRes == else" );
-                    return true;
+                default:
+                    return false;
                 }
             }
         }
-        Log.i(getText(R.string.log_tag).toString(), "authRes != any" );
         throw new IllegalStateException("Timed out");
+    }
+
+    private void destroy()
+    {
+        // Stop the previous session by interrupting the thread.
+        if (thread != null) {
+            thread.interrupt();
+            thread = null;
+        }
+
+        if (ifd != null && ifd.getFd() >= 0) {
+            try {
+                ifd.close();
+            } catch (Exception e) {
+                Log.i(getText(R.string.log_tag).toString(), e.getClass().toString() +
+                        "configure()->try->catch(Exception): " + e.getMessage());
+            }
+        }
     }
 
     private void configure()
     {
-        Builder builder = new Builder();
-        builder.setMtu(1400)
+        ifd = new Builder()
+            .setMtu(1400)
             .setSession(getText(R.string.app).toString())
-            .setConfigureIntent(configIntent)
+            .setConfigureIntent(getConfigIntent())
             .addRoute("0.0.0.0", 0)
-            .addAddress("10.0.0.2", 32);
-        if (ifd.getFd() >= 0) {
-            try {
-                ifd.close();
-            } catch (Exception e) {
-                // ignore
-            }
-        }
-        ifd = builder.establish();
+            .addAddress("10.0.0.2", 32)
+            .establish();
     }
 }
